40d930a10a78b91997211715dd72ae6d2be3ea6d
Krina Chauhan <krinarchauhan65@gmail.com>
2024-06-03 18:40:56 +0530

    add build folder

---
+++ client/build/static/js/758.af2fdf73.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[758],{9758:(e,n,r)=>{r.r(n),r.d(n,{default:()=>a});r(5043);var s=r(6446),l=r(5865),t=r(3216),c=r(579);const a=()=>{const e=(0,t.r5)();return console.log(e),(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(s.A,{style:{marginLeft:250},children:(0,c.jsx)(l.A,{children:"There was an error loading this page"})})})}}}]);
+//# sourceMappingURL=758.af2fdf73.chunk.js.map
